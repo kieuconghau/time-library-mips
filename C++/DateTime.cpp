a4c6f636d1b633bbbd7d47cc3c664dabb3a290ad
Kiều Công Hậu <46527188+kieuconghau@users.noreply.github.com>
2019-12-13 17:42:37 +0700

    Update strToInt
    More simple for transfering to MIPS

--- C++/DateTime.cpp
+++ C++/DateTime.cpp
@@ -5,7 +5,9 @@ int strToInt(char * str_input, int n) {
 
 	for (int i = 0; i < n; i++) {
 		if (str_input[i] >= 48 && str_input[i] <= 57) {
-			result += (str_input[i] - 48) * pow(10, n - i - 1); // ASCII: '0' = 48 
+			//result += (str_input[i] - 48) * pow(10, n - i - 1); // ASCII: '0' = 48
+			result *= 10;
+			result += str_input[i] - 48;
 		}
 	}
 
