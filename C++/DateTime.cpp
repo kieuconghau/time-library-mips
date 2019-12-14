f4563b062e2076d71678b044efa482dc24d4d264
Kiều Công Hậu <46527188+kieuconghau@users.noreply.github.com>
2019-12-14 20:58:55 +0700

    Update func isLeap
    More simple for transfering to MIPS

--- C++/DateTime.cpp
+++ C++/DateTime.cpp
@@ -83,8 +83,11 @@ int Year(char* TIME) {
 int isLeap(int year) {
 	if (year % 400 == 0)
 		return 1;
-
-	if (year % 4 == 0 && year % 100 != 0)
+	
+	if (year % 100 == 0)
+		return 0;
+	
+	if (year % 4 == 0)
 		return 1;
 
 	return 0;
