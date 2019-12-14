0c42b53935aad485a6bdc039e7bb70b1ca4255ac
Kiều Công Hậu <kieuconghau.it@gmail.com>
2019-12-14 21:36:01 +0700

    Add: func IsLeap | Bug fix: func Day, Month, Year

--- project2.asm
+++ project2.asm
@@ -1,11 +1,14 @@
 ##### int main()
 .data
-str:	.asciiz "13579"
+str:	.asciiz "18/12/0200"
 .text
 Main:
 	la  $a0, str
-	li  $a1, 5
-	jal StrToInt
+	#li  $a1, 10
+	jal Year
+	
+	move $a0, $v0
+	jal  IsLeap
 	
 	move $a0, $v0
 	li   $v0, 1
@@ -18,8 +21,8 @@ Main:
       	
 ##### int StrToInt(char* str_input, int n)
 StrToInt:
-						# $a0: str_input
-						# $a1: n
+						# $a0: str_input (char*)
+						# $a1: n (int)
 	addi $v0, $zero, 0			# $v0: result = 0
 	addi $t0, $zero, 0			# $t0: i = 0
 	
@@ -44,25 +47,74 @@ StrToInt:
 	
 ##### int Day(char* TIME)
 Day:
+						# $a0: TIME (char*)
+	addi $sp, $sp, -4
+	sw   $ra, 0($sp)
+	
 						# $a0: TIME
 	addi $a1, $zero, 2			# $a1: 2
 	jal  StrToInt
+	
+	lw   $ra, 0($sp)
+	addi $sp, $sp, 4
 	jr   $ra
+	
 
 
 ##### int Month(char* TIME)
 Month:
-						# $a0: TIME
+						# $a0: TIME (char*)
+	addi $sp, $sp, -4
+	sw   $ra, 0($sp)
+	
 	addi $a0, $a0, 3			# $a0: TIME + 3
 	addi $a1, $zero, 2			# $a1: 2
 	jal  StrToInt
+	
+	lw   $ra, 0($sp)
+	addi $sp, $sp, 4
 	jr   $ra
 	
 
 ##### int Year(char* TIME)
 Year:
-						# $a0: TIME
+						# $a0: TIME (char*)
+	addi $sp, $sp, -4
+	sw   $ra, 0($sp)
+	
 	addi $a0, $a0, 6			# $a0: TIME + 6
 	addi $a1, $zero, 4			# $a1: 4
 	jal  StrToInt
+	
+	lw   $ra, 0($sp)
+	addi $sp, $sp, 4
 	jr   $ra
+
+
+##### int IsLeap(int year)
+IsLeap:
+						# $a0: year (int)
+	addi $t0, $zero, 400			# $t0: temp0 = 400
+	div  $a0, $t0
+	mfhi $t0				# $t0: temp0 = year % 400
+	beq  $t0, $zero, IsLeap_return_1	# if temp0 == 0 then return 1
+	
+	addi $t0, $zero, 100			# $t0: temp0 = 100
+	div  $a0, $t0				
+	mfhi $t0				# $t0: temp0 = year % 100
+	beq  $t0, $zero, IsLeap_return_0	# if temp0 == 0 then return 0
+	
+	addi $t0, $zero, 4			# $t0: temp0 = 4
+	div  $a0, $t0
+	mfhi $t0				# $t0: temp0 = year % 4
+	beq  $t0, $zero, IsLeap_return_1	# if temp0 == 0 then return 1
+	
+	IsLeap_return_0:			# return 0
+	addi $v0, $zero, 0
+	jr   $ra
+	
+	IsLeap_return_1:			# return 1
+	addi $v0, $zero, 1
+	jr   $ra
+
+
