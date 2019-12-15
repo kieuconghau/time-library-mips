68a7a03460a6d473e04c184f1ba02c4217efc092
hoangnhan1105 <nguyenhoangnhan0@gmail.com>
2019-12-15 12:48:32 +0700

    Write input routine
    InputDate is 100% completed. CheckMonth is also finished. Currently working on CheckDay. After finishing it, gotta test InputDate too. Hopefully can finish all of them and move on to Date routine in this afternoon.

--- src/Project2.asm
+++ src/Project2.asm
@@ -1,10 +1,12 @@
 # Main routine
 	.data
+TIME: .space 10
 	.text
 	.globl Main
 Main:
-	jal InputDate
-	
+	la   $a0, TIME
+	jal  InputDate
+
 	# Exit
 	addi $v0, $zero, 10
 	syscall
@@ -15,17 +17,20 @@ Main:
 prompt1: .asciiz "Nhap ngay DAY: "
 prompt2: .asciiz "Nhap thang MONTH: "
 prompt3: .asciiz "Nhap nam YEAR: "
+error:   .asciiz "Error: Invalid input."
 day:     .space 3
 month:   .space 3
 year:    .space 5
 	.text
 InputDate:
-	addi $sp, $sp, -16
-	sw   $s0, 0($sp)
-	sw   $s1, 4($sp)
-	sw   $s2, 8($sp)
-	sw   $ra, 12($sp)
+	addi $sp, $sp, -20
+	sw   $a0, 0($sp)
+	sw   $ra, 4($sp)
+	sw   $s0, 8($sp)
+	sw   $s1, 12($sp)
+	sw   $s2, 16($sp)
 
+InputDate_input:
 	# Prompt user to input day
 	la   $a0, prompt1
 	addi $v0, $zero, 4
@@ -59,7 +64,7 @@ InputDate:
 	addi $v0, $zero, 8
 	syscall
 
-InputDate_while:
+
 	# Find the length of inputted 'year' string
 	la   $a0, year
 	addi $a1, $zero, 5
@@ -79,36 +84,101 @@ InputDate_while:
 	add  $s0, $v0, $zero
 
 
+	# Find the length of inputted 'month' string
+	la   $a0, month
+	addi $a1, $zero, 3
+	jal  StringLength
+	add  $s1, $v0, $zero
 
-#	la   $a0, year
-#	addi $a1, $zero, 4
-#	jal  CheckYear
-#	beq  $v0, $zero, InputDate_error
+	# Check if the inputted 'month' is a number
+	la   $a0, month
+	add  $a1, $s1, $zero
+	jal  CheckInput
+	beq  $v0, $zero, InputDate_error
 
-#	la   $a0, year
-#	addi $a1, $zero, 4
-#	jal  CheckYear
-#	beq  $v0, $zero, InputDate_error
+	# Convert 'month' from string to int
+	la   $a0, month
+	add  $a1, $s1, $zero
+	jal  StrToInt
+	add  $s1, $v0, $zero
 
-#	la   $a0, year
-#	addi $a1, $zero, 4
-#	jal  CheckYear
-#	beq  $v0, $zero, InputDate_error
+	# Check if 'month' is a valid month
+	add  $a0, $s1, $zero
+	jal  CheckMonth
+	beq  $v0, $zero, InputDate_error
+
+
+	# Find the length of inputted 'day' string
+	la   $a0, day
+	addi $a1, $zero, 3
+	jal  StringLength
+	add  $s2, $v0, $zero
+
+	# Check if the inputted 'day' is a number
+	la   $a0, day
+	add  $a1, $s2, $zero
+	jal  CheckInput
+	beq  $v0, $zero, InputDate_error
+
+	# Convert 'day' from string to int
+	la   $a0, day
+	add  $a1, $s2, $zero
+	jal  StrToInt
+	add  $s2, $v0, $zero
+
+	# Check if 'day' is a valid day
+	add  $a0, $s2, $zero
+	add  $a1, $s1, $zero
+	add  $a2, $s0, $zero
+	jal  CheckDay
+	beq  $v0, $zero, InputDate_error
+
+	j    InputDate_store
 
 InputDate_error:
+	# Print error message
+	la   $a0, error
+	addi $v0, $zero, 4
+	syscall
 
+	j    InputDate_input
 
+InputDate_store:
+	# Load TIME address from stack
+	lw   $a0, 0($sp)
+	add  $t0, $a0, $zero
 
-	lw   $s0, 0($sp)
-	lw   $s1, 4($sp)
-	lw   $s2, 8($sp)
-	lw   $ra, 12($sp)
-	addi $sp, $sp, 16
+	# Call Date routine
+	add  $a0, $s2, $zero
+	add  $a1, $s1, $zero
+	add  $a2, $s0, $zero
+	add  $a3, $t0, $zero
+	jal  Date
+	add  $v0, $v0, $zero
+
+	# Restore $ra, $s0, $s1, $s2 and $sp
+	lw   $ra, 4($sp)
+	lw   $s0, 8($sp)
+	lw   $s1, 12($sp)
+	lw   $s2, 16($sp)
+	addi $sp, $sp, 20
+
+	# Return
+	jr   $ra
+
+########################################
+	.data
+	.text
+Date:
 	jr   $ra
 
 ########################################
 # Check day validity
+# $a0: day in number
+# $a1: month in number
+# $a2: year in number
 	.data
+day_in_month: .word 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
 	.text
 CheckDay:
 
@@ -117,11 +187,16 @@ CheckDay:
 
 ########################################
 # Check month validity
+# $a0: month in number
 	.data
 	.text
 CheckMonth:
-
-
+	add  $v0, $zero, $zero
+	addi $t0, $zero, 12
+	slt  $t1, $t0, $a0
+	beq  $t1, $zero, CheckMonth_return
+	addi $v0, $zero, 1
+CheckMonth_return:
 	jr   $ra
 
 ########################################
@@ -206,4 +281,10 @@ StringLength_return:
 	.data
 	.text
 StrToInt:
+	jr   $ra
+
+########################################
+	.data
+	.text
+LeapYear:
 	jr   $ra
