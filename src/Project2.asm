da37b8444b28c54fe2b442a556036ab7e240f94b
hoangnhan1105 <nguyenhoangnhan0@gmail.com>
2019-12-15 19:08:44 +0700

    Write input routine
    Borrow StrToInt routine from KCH

--- src/Project2.asm
+++ src/Project2.asm
@@ -318,10 +318,30 @@ StringLength_while:
 StringLength_return:
 	jr   $ra
 
-########################################
-	.data
-	.text
+##### int StrToInt(char* str_input, int n)
+# Written by KCH
 StrToInt:
+						# $a0: str_input (char*)
+						# $a1: n (int)
+	addi $v0, $zero, 0			# $v0: result = 0
+	addi $t0, $zero, 0			# $t0: i = 0
+	
+	StrToInt_loop:
+	slt  $t1, $t0, $a1
+	beq  $t1, $zero, StrToInt_end		# if i >= n then exit loop
+	
+	add  $t1, $a0, $t0			# str_input + i
+	lb   $t1, 0($t1)			# $t1: str_input[i]
+	
+	addi $t2, $zero, 10
+	mul  $v0, $v0, $t2			# result *= 10
+	addi $t1, $t1, -48			# str_input[i] -= '0'
+	add  $v0, $v0, $t1			# result += str_input[i]
+	
+	addi $t0, $t0, 1			# ++i
+	j    StrToInt_loop
+	
+	StrToInt_end:
 	jr   $ra
 
 ##### int IsLeap(int year)
